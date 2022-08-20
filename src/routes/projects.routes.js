9b13ec072d700c8ef7c470458cd7667338a1f9f9
joaquin-labrador <joaquinlabrador1010@gmail.com>
2022-08-19 23:36:47 -0300

    added some endpionts 👀

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import projectsController from "../controller/projects.controller.js";
+import projectAndTasksController from "../controller/project&tasks.controller.js";
 const router = Router();
 
 router.get("/", projectsController.getProjects);
@@ -8,6 +9,6 @@ router.put("/:id", projectsController.updateProject);
 router.delete("/:id", projectsController.deleteProject);
 router.get("/:id", projectsController.getProjectById);
 
-router.get("/:id/tasks", projectsController.getTasksByProjectId);
+router.get("/:id/tasks", projectAndTasksController.getTasksByProjectId);
 
 export default router;
