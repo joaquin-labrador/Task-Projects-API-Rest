35e1eb47b20a75b18f984a9df50a34c87fc43d26
joaquin-labrador <joaquinlabrador1010@gmail.com>
2022-08-19 23:16:49 -0300

    Added ✅ endpoint for relation of Task & Project

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import projectsController from "../controller/projects.controller.js";
 const router = Router();
 
-router.get("/projects", projectsController.getProjects);
-router.post("/projects", projectsController.createProject);
-router.put("/projects/:id", projectsController.updateProject);
-router.delete("/projects/:id", projectsController.deleteProject);
-router.get("/projects/:id", projectsController.getProjectById);
+router.get("/", projectsController.getProjects);
+router.post("/", projectsController.createProject);
+router.put("/:id", projectsController.updateProject);
+router.delete("/:id", projectsController.deleteProject);
+router.get("/:id", projectsController.getProjectById);
+
+router.get("/:id/tasks", projectsController.getTasksByProjectId);
 
 export default router;
